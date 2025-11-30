1896f35ec040a67e20706b33c63d255474f7575f
Wilker Ribeiro <rekliw97@gmail.com>
2025-11-29 23:10:44 -0300

    feat: optimize edge delivery configuration

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.r2.cloudflarestorage.com',
+        hostname: '**',
       },
     ],
   },
